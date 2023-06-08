5281f6ee367853daac81de8fe4c5905636ae7957
Mahesh Babu <mahesh.b.pec@gmail.com>
2023-06-08 10:39:28 +0530

    Initial commit

---
+++ src/main/java/com/maheshbabu/ff4jSpringApplication/Ff4jSpringApplication.java
@@ -0,0 +1,13 @@
+package com.maheshbabu.ff4jSpringApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ff4jSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ff4jSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maheshbabu/ff4jSpringApplication/Ff4jSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maheshbabu.ff4jSpringApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ff4jSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
